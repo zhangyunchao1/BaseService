68038d6a20b80dc0a3214d4c42c90bca247fcffe
zhangyunchao <zhangyunchao@wanda.cn>
2017-07-24 17:33:07 +0800

    [ADD]:BaseService工程搭建@

---
+++ BaseService/BaseService/BaseService.h
@@ -0,0 +1,13 @@
+//
+//  BaseService.h
+//  BaseService
+//
+//  Created by chao on 2017/7/24.
+//  Copyright © 2017年 chao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseService : NSObject
+
+@end
---
+++ BaseService/BaseService/BaseService.m
@@ -0,0 +1,13 @@
+//
+//  BaseService.m
+//  BaseService
+//
+//  Created by chao on 2017/7/24.
+//  Copyright © 2017年 chao. All rights reserved.
+//
+
+#import "BaseService.h"
+
+@implementation BaseService
+
+@end
